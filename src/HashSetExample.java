30c140b1d0bd38d8e7646feded846129f1d6857a
Arulmurugan <arulmurugansk7@gmail.com>
2022-07-13 00:43:10 +0530

    Hashmap class added

--- src/HashSetExample.java
+++ src/HashSetExample.java
@@ -1,35 +1,24 @@
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.LinkedHashSet;
 
 public class HashSetExample {
 
     public static void main(String args[]) {
-
-        // Creating an empty LinekdhashSet of string type
         LinkedHashSet<String> lhs = new LinkedHashSet<>();
-
-        // Adding elements to the above Set
-        // by invoking the add() method
         lhs.add("Java");
         lhs.add("T");
         lhs.add("Point");
         lhs.add("Good");
         lhs.add("Website");
-
-        // displaying all the elements on the console
         System.out.println("The hash set is: " + lhs);
-
-        // Removing an element from the above linked Set
-
-        // since the element "Good" is present, therefore, the method remove()
-        // returns true
         System.out.println(lhs.remove("Good"));
-
-        // After removing the element
         System.out.println("After removing the element, the hash set is: " + lhs);
-
-        // since the element "For" is not present, therefore, the method remove()
-        // returns false
         System.out.println(lhs.remove("For"));
+        Object[] lhs1 = lhs.toArray();
 
+        for (Object i : lhs1) {
+            System.out.print(i + " ");
+        }
     }
 }
